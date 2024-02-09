1bb123c3c3229ca0f0d1144aa90c48afbb87b303
iyo143 <limlimgo14@gmail.com>
2024-02-09 19:17:32 +0800

    finish the why page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from "@/views/HomePage.vue"
 import WhatView from "@/views/WhatPage.vue"
+import WhyView from "@/views/WhyPage.vue"
 Vue.use(VueRouter)
 
 const routes = [
@@ -14,6 +15,11 @@ const routes = [
     path:'/what', 
     name:'what', 
     component: WhatView
+  },
+  {
+    path:'/why', 
+    name:'why', 
+    component: WhyView
   },
 ]
 
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,51 +1,128 @@
 <template>
     <transition>
         <div class="container">
-            <div class="half-back"></div>
-            <NavBar />
-            <section class="land-page">
-                <div class="home-links">
-                    <div class="what title">
-                        <ion-icon name="chevron-back-outline"></ion-icon>
-                        <p>What</p>
-                    </div>
-                    <div class="who">
-                        <p>Who?</p>
-                    </div>
-                    <div class="why title">
-                        <p>Why</p>
-                        <ion-icon name="chevron-forward-outline"></ion-icon>
-                    </div>
-                </div>
-                <div class="bottom-arrow">
-                    <ion-icon name="chevron-down-outline"></ion-icon>
-                </div>
-            </section>
-            <section class="info">
-                <div class="p-info">
-                    <div>
-                        <p class="p1">This is my Personal Portfolio</p>
-                        <p class="p2">I’m Rio Martin Golimlim</p>
-                        <div class="skills">
-                            <p>&lt;Back-End&gt;</p>
-                            <span class="circle"></span>
-                            <p class="mid">UX/UI Designer</p>
-                            <span class="circle"></span>
-                            <p>&lt;Front-End&gt;</p>
+            <div class="half-back" :class="{ isWhy: page == 'why' }">
+                <div class="why_container" v-if="page == 'why'">
+                    <div class="why_landpage">
+                        <div class="why_left">
+                            <img src="@/assets/images/profile.png" alt="" />
+                        </div>
+                        <div class="why_right">
+                            <h1>Why Me?</h1>
+                            <p>
+                                Because Im Good at Finding solutions I read
+                                stack overflow solutions even if they are not
+                                checked And lastly I am subscribed and up to
+                                date to many Indian youtubers and also to other
+                                web developers
+                            </p>
+                            <div class="btn gold-light">Skills</div>
+                            <div class="btm-arrow">
+                                <img src="@/assets/images/down.png" alt="" />
+                            </div>
                         </div>
                     </div>
-                    <div class="btn-group">
-                        <div class="btn gold-dark" @click="showModal = true">
-                            Hire Me!
+                    <section class="why_skills">
+                        <h1>HERE ARE MY SKILLS</h1>
+                        <div class="cards">
+                            <div class="card">
+                                <img src="@/assets/images/db.png" alt="" />
+                                <h2>BACK-END<br/>DEVELOPER</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit. Faucibus nulla at ultrices
+                                    volutpat at. Phasellus risus nunc risus,
+                                    iaculis massa, dolor rutrum. Gravida leo
+                                    tristique egestas quis urna, ut aliquam
+                                    nascetur. Nullam posuere urna nisl turpis
+                                    enim, pellentesque non arcu.
+                                </p>
+                                <p>INTERMEDIATE</p>
+                            </div>
+                            <div class="card">
+                                <img src="@/assets/images/windows2.png" alt="" />
+                                <h2>UI/UX<br/> DESIGNER</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit. Faucibus nulla at ultrices
+                                    volutpat at. Phasellus risus nunc risus,
+                                    iaculis massa, dolor rutrum. Gravida leo
+                                    tristique egestas quis urna, ut aliquam
+                                    nascetur. Nullam posuere urna nisl turpis
+                                    enim, pellentesque non arcu.
+                                </p>
+                                <p>INTERMEDIATE</p>
+                            </div>
+                            <div class="card">
+                                <img src="@/assets/images/code.png" alt="" />
+                                <h2>FRONT-END <br/>DEVELOPER</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit. Faucibus nulla at ultrices
+                                    volutpat at. Phasellus risus nunc risus,
+                                    iaculis massa, dolor rutrum. Gravida leo
+                                    tristique egestas quis urna, ut aliquam
+                                    nascetur. Nullam posuere urna nisl turpis
+                                    enim, pellentesque non arcu.
+                                </p>
+                                <p>INTERMEDIATE</p>
+                            </div>
                         </div>
-                        <div class="btn gold-light">Contact Me!</div>
+                    </section>
+                </div>
+            </div>
+            <NavBar @goTo="goToPage($event)" />
+            <div class="who_container" v-if="page == 'who'">
+                <section class="land-page">
+                    <div class="home-links">
+                        <div class="what title" @click="page = 'what'">
+                            <ion-icon name="chevron-back-outline"></ion-icon>
+                            <p>What</p>
+                        </div>
+                        <div class="who">
+                            <p>Who?</p>
+                        </div>
+                        <div class="why title" @click="page = 'why'">
+                            <p>Why</p>
+                            <ion-icon name="chevron-forward-outline"></ion-icon>
+                        </div>
                     </div>
-                </div>
-                <div class="blob">
-                    <img src="@/assets/images/blob.png" alt="" />
-                </div>
-                <HireModal v-if="showModal" @closeModal="showModal = false" />
-            </section>
+                    <div class="bottom-arrow">
+                        <ion-icon name="chevron-down-outline"></ion-icon>
+                    </div>
+                </section>
+                <section class="info">
+                    <div class="p-info">
+                        <div>
+                            <p class="p1">This is my Personal Portfolio</p>
+                            <p class="p2">I’m Rio Martin Golimlim</p>
+                            <div class="skills">
+                                <p>&lt;Back-End&gt;</p>
+                                <span class="circle"></span>
+                                <p class="mid">UX/UI Designer</p>
+                                <span class="circle"></span>
+                                <p>&lt;Front-End&gt;</p>
+                            </div>
+                        </div>
+                        <div class="btn-group">
+                            <div
+                                class="btn gold-dark"
+                                @click="showModal = true"
+                            >
+                                Hire Me!
+                            </div>
+                            <div class="btn gold-light">Contact Me!</div>
+                        </div>
+                    </div>
+                    <div class="blob">
+                        <img src="@/assets/images/blob.png" alt="" />
+                    </div>
+                    <HireModal
+                        v-if="showModal"
+                        @closeModal="showModal = false"
+                    />
+                </section>
+            </div>
         </div>
     </transition>
 </template>
@@ -60,9 +137,15 @@ export default {
     },
     setup() {
         const showModal = ref(false);
-
+        const page = ref("why");
+        const goToPage = (p) => {
+            console.log(page);
+            page.value = p;
+        };
         return {
             showModal,
+            goToPage,
+            page,
         };
     },
 };
@@ -76,12 +159,11 @@ export default {
         transform: translateY(10px);
     }
 }
+
 .container {
     background: #041c32;
-    padding: 0;
-    margin: 0;
     box-sizing: border-box;
-    min-height: 100vh;
+    height: 100%;
     .half-back {
         width: 50%;
         height: 100%;
@@ -89,9 +171,110 @@ export default {
         right: 0;
         background: #064663;
         border-radius: 0px 0px 0px 355px;
+        transition: 0.3s;
+        .why_landpage {
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            padding: 0 150px;
+            height: 100vh;
+            .why_right {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                gap: 40px;
+
+                h1 {
+                    font-size: 50px;
+                    color: #ecb365;
+                    text-align: center;
+                }
+                p {
+                    width: 590px;
+                    text-align: center;
+                    font-size: 30px;
+                    color: #fff;
+                    font-weight: lighter;
+                }
+
+                .btn {
+                    display: flex;
+                    width: 180px;
+                    height: 57px;
+                    border-radius: 10px;
+                    justify-content: center;
+                    align-items: center;
+                    font-size: 20px;
+                    cursor: pointer;
+                    transition: 0.2s;
+                }
+                .gold-light {
+                    border: 2px solid #ecb365;
+                    color: #ecb365;
+                    &:hover {
+                        background: #ecb365;
+                        color: #04293a;
+                    }
+                }
+                .btm-arrow {
+                    display: flex;
+                    justify-content: center;
+                    animation-name: upDown;
+                    animation-iteration-count: infinite;
+                    animation-direction: alternate;
+                    animation-duration: 0.7s;
+                }
+            }
+        }
+        .why_skills {
+            background: #064663;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            padding: 0 150px;
+            gap:50px;
+            h1{
+                font-size: 50px;
+                color: #ECB365;
+            }
+            .cards {
+                display: flex;
+                justify-content: space-between;
+                .card {
+                    padding: 40px;
+                    width: 387px;
+                    height: 452px;
+                    box-shadow: 0 0 30px #000;
+                    border-radius: 20px;
+                    border-bottom: 10px solid #ECB365;
+                    h2{
+                        font-size: 20px;
+                        color: #ECB365;
+                        margin: 30px 0 30px;
+                    }
+                    p{
+                        color: #fff;
+                        font-weight: lighter;
+                        &:last-child{
+                            color: #ecb365;
+                            font-weight: bolder;
+                            margin-top: 80px;
+                            font-size: 15px;
+                        }
+                    }
+
+                   
+                }
+            }
+        }
+    }
+    .isWhy {
+        width: 100%;
+        border-radius: 0px 0px 0px 0px;
     }
     .land-page {
-        height: 100vh;
         .home-links {
             display: flex;
             padding: 0 80px;
@@ -218,6 +401,10 @@ export default {
                 }
             }
         }
+    }
+
+    .why_landpage {
+        background: #064663;
     }
 }
 </style>
